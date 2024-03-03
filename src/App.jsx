4f72a9b52b3eb192e6ce7b688022a62146ce2f35
imlakshaykumar <imlakshaykumar@gmail.com>
2024-03-04 04:56:20 +0530

    initial foundation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import { NavBar } from "./components/NavBar";
+import { NavBar } from "./components/global/NavBar";
+import { Footer } from "./components/global/Footer";
 
 function App() {
   return (
     <>
       <NavBar />
       <Routes>{/* <Route path="/" element={}/> */}</Routes>
+      <Footer />
     </>
   );
 }
