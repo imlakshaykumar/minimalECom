20a0dba6d099c3ae7be2ebf244f09afe68f6c553
Lakshay kumar <imlakshaykumar@gmail.com>
2024-03-21 05:28:24 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import { NavBar } from "./components/global/NavBar";
-import { Footer } from "./components/global/Footer";
+import { Home } from "../pages/Home";
+// import { Footer } from "./components/global/Footer";
 
 function App() {
   return (
     <>
       <NavBar />
-      <Routes>{/* <Route path="/" element={}/> */}</Routes>
-      <Footer />
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+      </Routes>
+      {/* <Footer /> */ }
     </>
   );
 }
--- src/components/global/NavBar.jsx
+++ src/components/global/NavBar.jsx
@@ -1,7 +1,22 @@
+import logo from '/vite.svg'
+
 export const NavBar = () => {
   return (
-    <div>
-      <div>NavBar</div>
+    <div className='w-full flex justify-center shadow-md'>
+      <div className='flex w-[1200px] justify-between items-center px-8 py-6'>
+        <div className='logo-div object-cover cursor-pointer'>
+          <img src={ logo } alt="logo" className='logo w-[60px] h-auto block' />
+        </div>
+        <div className="nav-items flex gap-7 justify-between items-center">
+          <ul className="nav-items-list flex gap-7 w-full justify-evenly uppercase items-center">
+            <li className="item cursor-pointer hover:underline font-medium">categories</li>
+            <li className="item cursor-pointer hover:underline font-medium">product page</li>
+          </ul>
+          <div className="cart-logo-div cursor-pointer object-cover">
+            <i data-array-length="0" className="cart-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="tabler-icon tabler-icon-shopping-cart"><path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path><path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path><path d="M17 17h-11v-14h-2"></path><path d="M6 5l14 1l-1 7h-13"></path></svg></i>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
