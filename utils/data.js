6be87f82e1b5a0bf548b6732d6212543d3ba4156
Lakshay kumar <imlakshaykumar@gmail.com>
2024-03-21 15:30:33 +0530

    added product data

---
+++ utils/data.js
@@ -0,0 +1,123 @@
+import image1 from '../src/assets/static/products/1.122c04e77ef5da9e8406.jpg'
+import image2 from '../src/assets/static/products/2.f312c91f99a2c3a60550.jpg'
+import image3 from '../src/assets/static/products/3.4c8f91797d8ef5e0206c.png'
+import image4 from '../src/assets/static/products/4.download.jpeg'
+import image5 from '../src/assets/static/products/5.e2e1c208d1932b2c0db0.jpg'
+import image6 from '../src/assets/static/products/6.5644c4636f0b3f050bf7.jpg'
+import image7 from '../src/assets/static/products/7.download (1).jpeg'
+import image8 from '../src/assets/static/products/8.35f833ef20163491e804.jpg'
+import image9 from '../src/assets/static/products/9.c6d969e8587dd1042095.png'
+import image10 from '../src/assets/static/products/10.01ad0d2d385553f6ea92.jpg'
+import image11 from '../src/assets/static/products/11.download (2).jpeg'
+import image12 from '../src/assets/static/products/12.ce8de8e38ff2dc475ed6.png'
+import image13 from '../src/assets/static/products/13.3e405d8959216287316c.jpg'
+import image14 from '../src/assets/static/products/14.a674c36b5a4aa0bb7377.png'
+import image15 from '../src/assets/static/products/15.34062d24a3b098cdc795.jpg'
+import image16 from '../src/assets/static/products/16.9d0e830b5f094893fe86.jpg'
+import image17 from '../src/assets/static/products/17.b04524e5024f75206ab9.png'
+import image18 from '../src/assets/static/products/18.409250837a90245a30b9.png'
+import image19 from '../src/assets/static/products/19.1e5832c36583dbb804f2.jpg'
+import image20 from '../src/assets/static/products/20r.8cde710dbf28ebd08941.jpg'
+
+export const data = [
+    {
+        image: image1,
+        name: "Little American Sheepskin",
+        price: "$89"
+    },
+    {
+        image: image2,
+        name: "Pop-up toaster",
+        price: "$19"
+    },
+    {
+        image: image3,
+        name: "Lamp light blue",
+        price: "$32"
+    },
+    {
+        image: image4,
+        name: "Pulp unit 5-compartments",
+        price: "$54"
+    },
+    {
+        image: image5,
+        name: "Golden modern light",
+        price: "$43"
+    },
+    {
+        image: image6,
+        name: "Body oil 200ml",
+        price: "$74"
+    },
+    {
+        image: image7,
+        name: "Black and white lamp",
+        price: "$10"
+    },
+    {
+        image: image8,
+        name: "Shelf a black ash/black",
+        price: "$82"
+    },
+    {
+        image: image9,
+        name: "Cube lolo vase black",
+        price: "$89"
+    },
+    {
+        image: image10,
+        name: "Traditional Armchair",
+        price: "$19"
+    },
+    {
+        image: image11,
+        name: "The sun pendant black",
+        price: "$32"
+    },
+    {
+        image: image12,
+        name: "Simple golden lamp",
+        price: "$54"
+    },
+    {
+        image: image13,
+        name: "unbleached cotton pads 120",
+        price: "$43"
+    },
+    {
+        image: image14,
+        name: "Oak spanish chair",
+        price: "$74"
+    },
+    {
+        image: image15,
+        name: "Copanhagen armchair",
+        price: "$10"
+    },
+    {
+        image: image16,
+        name: "Anti dark light",
+        price: "$82"
+    },
+    {
+        image: image17,
+        name: "table lamp",
+        price: "$43"
+    },
+    {
+        image: image18,
+        name: "mat black lamp",
+        price: "$74"
+    },
+    {
+        image: image19,
+        name: "confy chair",
+        price: "$10"
+    },
+    {
+        image: image20,
+        name: "body oil 50ml",
+        price: "$82"
+    }
+]
