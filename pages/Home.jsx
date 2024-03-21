f4d778a391c9ec2da4286520a0117404549d5083
Lakshay kumar <imlakshaykumar@gmail.com>
2024-03-21 15:32:55 +0530

    added productSection

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,10 +1,16 @@
+import { Banner1 } from "../src/components/Banner1"
+import { Banner2 } from "../src/components/Banner2"
 import { HomeSection } from "../src/components/HomeSection"
+import { ProudSection } from "../src/components/ProudSection"
 
 export const Home = () => {
     return (
         <>
             <main>
                 <HomeSection />
+                <ProudSection />
+                <Banner1 />
+                <Banner2 />
             </main>
         </>
     )
---
+++ src/components/ProudSection.jsx
@@ -0,0 +1,33 @@
+import { data } from "../../utils/data"
+
+export const ProudSection = () => {
+
+    const slicedData = data.slice(0, 8);
+
+    return (
+        <>
+            <div className="container max-w-[1200px] mx-auto my-16 px-5">
+                <div className="heading-div w-full mb-10">
+                    <h3 className="heading text-2xl font-bold">Products we are proud of</h3>
+                </div>
+                <div className="products-div grid grid-cols-4 gap-5">
+                    {
+                        slicedData?.map((item, key) => {
+                            return (
+                                <div key={ key } className="border-2 border-gray-300 hover:border-black transition-all duration-100 ease-in cursor-pointer">
+                                    <div className="item-image-div">
+                                        <img src={ item.image } alt="image1" className="item-image" />
+                                    </div>
+                                    <div className="item-info-div p-2">
+                                        <p className="item-name">{ item.name }</p>
+                                        <p className="item-price text-xl font-bold mt-3">{ item.price }</p>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </>
+    )
+}
