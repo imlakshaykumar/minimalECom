da9c3cdce873ded16539074a854d2ca23c3c6e2b
Lakshay kumar <imlakshaykumar@gmail.com>
2024-03-30 18:43:42 +0530

    fixed scroll-top

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import { NavBar } from "./components/global/NavBar";
 import { Home } from "./pages/Home";
 import { Footer } from "./components/global/Footer";
 import { Categories } from "./pages/Categories";
 import { useState } from "react";
 import { Product } from "./pages/Product";
+import { useEffect } from "react";
 
 function App() {
   let [category, setCategory] = useState("all");
   const navigate = useNavigate();
+
+  // when the route changes, always scroll on the top of the page
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
 
   const handleCategoryChange = (newCategory) => {
     setCategory(newCategory);
